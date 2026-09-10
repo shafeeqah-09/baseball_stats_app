04295c5e8186f7b84a8e81d92a417fba2ba9932f
Shafeeqah <shmoojhb025@student.wethinkcode.co.za>
2026-09-10 13:39:38 +0200

    feat: replace counter demo with Baseball Stats home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BaseballStatsApp extends StatelessWidget {
+  const BaseballStatsApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Baseball Stats',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +29,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        mainAxisAlignment: MainAxisAlignment.center,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Baseball Stats'),
+        backgroundColor: Theme.of(conetxt).colorScheme.inversePrimary,
+      ),
+      body: const Center(
+        child: Text(
+          'Baseball Stats - coming soon'
+              style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
